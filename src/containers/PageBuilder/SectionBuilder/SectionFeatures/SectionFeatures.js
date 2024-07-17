4337f4b32744c332ff75c43ce5897c1206841788
Andres Estrada <andres@theandino.com>
2024-07-17 14:28:54 -0500

    Change update fill video

--- src/containers/PageBuilder/SectionBuilder/SectionFeatures/SectionFeatures.js
+++ src/containers/PageBuilder/SectionBuilder/SectionFeatures/SectionFeatures.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { arrayOf, bool, func, node, object, shape, string } from 'prop-types';
-import classNames from 'classnames';
-
-import Field, { hasDataInFields } from '../../Field';
+import Field, { hasDataInFields } from '../../Field'; // Asegúrate de que esta importación esté correcta
 import BlockBuilder from '../../BlockBuilder';
-
 import SectionContainer from '../SectionContainer';
-
+import {VideoHero} from '../../../LandingPage/VideoHero';
+import classNames from 'classnames';
 import css from './SectionFeatures.module.css';
 
-// Section component that shows features.
-// Block content are shown in a row-like way:
-// [image] text
-// text [image]
-// [image] text
 const SectionFeatures = props => {
   const {
     sectionId,
@@ -29,8 +21,6 @@ const SectionFeatures = props => {
     options,
   } = props;
 
-  // If external mapping has been included for fields
-  // E.g. { h1: { component: MyAwesomeHeader } }
   const fieldComponents = options?.fieldComponents;
   const fieldOptions = { fieldComponents };
 
@@ -38,75 +28,41 @@ const SectionFeatures = props => {
   const hasBlocks = blocks?.length > 0;
 
   return (
-    <SectionContainer
-      id={sectionId}
-      className={className}
-      rootClassName={rootClassName}
-      appearance={appearance}
-      options={fieldOptions}
-    >
-      {hasHeaderFields ? (
-        <header className={defaultClasses.sectionDetails}>
-          <Field data={title} className={defaultClasses.title} options={fieldOptions} />
-          <Field data={description} className={defaultClasses.description} options={fieldOptions} />
-          <Field data={callToAction} className={defaultClasses.ctaButton} options={fieldOptions} />
-        </header>
-      ) : null}
-      {hasBlocks ? (
-        <div
-          className={classNames(defaultClasses.blockContainer, css.featuresMain, {
-            [css.noSidePaddings]: isInsideContainer,
-          })}
-        >
-          <BlockBuilder
-            rootClassName={css.block}
-            ctaButtonClass={defaultClasses.ctaButton}
-            blocks={blocks}
-            sectionId={sectionId}
-            responsiveImageSizes="(max-width: 767px) 100vw, 568px"
-            options={options}
-          />
-        </div>
-      ) : null}
-    </SectionContainer>
+    <div className={css.featuresSection}>
+      <VideoHero />
+      {/* <SectionContainer
+        id={sectionId}
+        className={className}
+        rootClassName={rootClassName}
+        appearance={appearance}
+        options={fieldOptions}
+      >
+        {hasHeaderFields ? (
+          <header className={defaultClasses.sectionDetails}>
+            <Field data={title} className={defaultClasses.title} options={fieldOptions} />
+            <Field data={description} className={defaultClasses.description} options={fieldOptions} />
+            <Field data={callToAction} className={defaultClasses.ctaButton} options={fieldOptions} />
+          </header>
+        ) : null}
+        {hasBlocks ? (
+          <div
+            className={classNames(defaultClasses.blockContainer, css.featuresMain, {
+              [css.noSidePaddings]: isInsideContainer,
+            })}
+          >
+            <BlockBuilder
+              rootClassName={css.block}
+              ctaButtonClass={defaultClasses.ctaButton}
+              blocks={blocks}
+              sectionId={sectionId}
+              responsiveImageSizes="(max-width: 767px) 100vw, 568px"
+              options={options}
+            />
+          </div>
+        ) : null}
+      </SectionContainer> */}
+    </div>
   );
-};
-
-const propTypeOption = shape({
-  fieldComponents: shape({ component: node, pickValidProps: func }),
-});
-
-SectionFeatures.defaultProps = {
-  className: null,
-  rootClassName: null,
-  defaultClasses: null,
-  textClassName: null,
-  title: null,
-  description: null,
-  appearance: null,
-  callToAction: null,
-  blocks: [],
-  isInsideContainer: false,
-  options: null,
-};
-
-SectionFeatures.propTypes = {
-  sectionId: string.isRequired,
-  className: string,
-  rootClassName: string,
-  defaultClasses: shape({
-    sectionDetails: string,
-    title: string,
-    description: string,
-    ctaButton: string,
-  }),
-  title: object,
-  description: object,
-  appearance: object,
-  callToAction: object,
-  blocks: arrayOf(object),
-  isInsideContainer: bool,
-  options: propTypeOption,
 };
 
 export default SectionFeatures;
