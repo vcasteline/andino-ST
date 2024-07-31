39e768c49707e4e8fe8a3d060c0dc7c8cc04e94b
RichyAndino <richard@theandino.com>
2024-07-31 16:41:22 -0500

    agregar validaciones en los inputs

--- src/components/FieldWeightDimensions/FieldWeightDimensions.jsx
+++ src/components/FieldWeightDimensions/FieldWeightDimensions.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import './FieldWeightDimensions.css'
 
 export const FieldWeightDimensions = ({ data, values }) => {
-    // console.log(values)
+    const [value, setValue] = useState('');
+    const [error, setError] = useState('');
+    console.log(data)
+
+    const handleChange = (e) => {
+        const inputValue = e.target.value;
+
+        // Expresión regular para validar números con dos decimales
+        const regex = /^\d+(\.\d{0,2})?$/;
+
+        if (regex.test(inputValue) || inputValue === '') {
+            setValue(inputValue);
+            setError('');
+        } else {
+            setError('invalid data, can only be numbers.');
+        }
+    };
     return (
         <>
             <label></label>
@@ -10,6 +26,7 @@ export const FieldWeightDimensions = ({ data, values }) => {
                 <div>
                     <label>Weight</label>
                     <input
+                        onChange={handleChange}
                         placeholder={data[6].key + " (lb)"} />
                 </div>
                 <i class="fas fa-box-open" style={{ fontSize: "7vh" }}></i>
@@ -17,15 +34,19 @@ export const FieldWeightDimensions = ({ data, values }) => {
                     <label>Dimensions</label>
                     <div className="containerDimensions">
                         <input
+                            onChange={handleChange}
                             placeholder={data[5].key + " (in)"} /><span>X</span>
                         <input
+                            onChange={handleChange}
                             placeholder={data[3].key + " (in)"} /><span>X</span>
                         <input
+                            onChange={handleChange}
                             placeholder={data[3].key + " (in)"} />
 
                     </div>
                 </div>
             </div>
+            {error && <span style={{ color: 'red' }}>{error}</span>}
         </>
     )
 }
