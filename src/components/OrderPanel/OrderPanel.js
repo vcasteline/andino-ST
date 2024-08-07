24d1b9058300be2fa8d23f05147981cfd1b70bfb
Andres Estrada <andres@theandino.com>
2024-08-07 10:01:49 -0500

    Cambios Rama

--- src/components/OrderPanel/OrderPanel.js
+++ src/components/OrderPanel/OrderPanel.js
@@ -327,7 +327,10 @@ const OrderPanel = props => {
   };
 
   return (
-    <div className={classes}>
+    <>
+    
+
+    <div className={css.stickyContainer}>
       <ModalInMobile
         containerClassName={css.modalContainer}
         id="OrderFormInModal"
@@ -336,6 +339,7 @@ const OrderPanel = props => {
         showAsModalMaxWidth={MODAL_BREAKPOINT}
         onManageDisableScrolling={onManageDisableScrolling}
         usePortal
+        
       >
         <div className={css.modalHeading}>
           <H1 className={css.heading}>{title}</H1>
@@ -349,7 +353,6 @@ const OrderPanel = props => {
           key={'quantityPriceBreaks-break'}
           quantityPriceBreaks={quantityPriceBreaks}
         />
-
         <div className={css.author}>
           <AvatarSmall user={author} className={css.providerAvatar} />
           <span className={css.providerNameLinked}>
@@ -405,28 +408,28 @@ const OrderPanel = props => {
                   </select>
                 </div>
               ))}
-              {selectedVariantFields?.length !== 0 &&
-                <>
-                  <div className={css.inputClose}>
-                    <input
-                      className={css.quantityField}
-                      type="number"
-                      min={1}
-                      value={currentVariant?.quantity}
-                      onChange={e => handleVariantChange('quantity', parseInt(e.target.value))}
-                      required
-                    />
-                  </div>
-                  <Button
-                    className={css.addVariantButton}
-                    onClick={handleAddVariant}
-                    disabled={selectedVariantFields?.some(field => !currentVariant[field])}
-                  >
-                    Add Variant
-                  </Button>
-                </>
+              {selectedVariantFields?.length !== 0 && 
+              <>
+              <div className={css.inputClose}>
+                <input
+                  className={css.quantityField}
+                  type="number"
+                  min={1}
+                  value={currentVariant?.quantity}
+                  onChange={e => handleVariantChange('quantity', parseInt(e.target.value))}
+                  required
+                />
+              </div>
+              <Button
+                className={css.addVariantButton}
+                onClick={handleAddVariant}
+                disabled={selectedVariantFields?.some(field => !currentVariant[field])}
+              >
+                Add Variant
+              </Button>
+              </>
               }
-
+              
             </div>
           </div>
         }
@@ -510,12 +513,6 @@ const OrderPanel = props => {
             categoryLevel3={categoryLevel3}
             selectedVariantFields={selectedVariantFields}
             publicData={publicData}
-            closeOrderModal={closeOrderModal}
-            history={history}
-            location={location}
-            fromTransactionPage={fromTransactionPage}
-            openOfferModal={openOfferModal}
-            offer={offer}
           />
         ) : showInquiryForm ? (
           <InquiryWithoutPaymentForm formId="OrderPanelInquiryForm" onSubmit={onSubmit} />
@@ -525,7 +522,9 @@ const OrderPanel = props => {
           </p>
         ) : null}
       </ModalInMobile>
-      <div className={css.openOrderForm}>
+
+    </div>
+    <div className={css.openOrderForm}>
         <PriceMaybe
           price={price}
           publicData={publicData}
@@ -540,55 +539,31 @@ const OrderPanel = props => {
             <FormattedMessage id="OrderPanel.closedListingButtonText" />
           </div>
         ) : (
-          <div style={{ width: '100%' }}>
-            <PrimaryButton
-              onClick={handleSubmit(
-                isOwnListing,
-                isClosed,
-                showInquiryForm,
-                onSubmit,
-                history,
-                location
-              )}
-              disabled={isOutOfStock}
-            >
-              {isBooking ? (
-                <FormattedMessage id="OrderPanel.ctaButtonMessageBooking" />
-              ) : isOutOfStock ? (
-                <FormattedMessage id="OrderPanel.ctaButtonMessageNoStock" />
-              ) : isPurchase ? (
-                <FormattedMessage id="OrderPanel.ctaButtonMessagePurchase" />
-              ) : (
-                <FormattedMessage id="OrderPanel.ctaButtonMessageInquiry" />
-              )}
-            </PrimaryButton>
-
-            <br />
-            <SecondaryButton
-              onClick={handleSubmit(
-                isOwnListing,
-                isClosed,
-                showInquiryForm,
-                onSubmit,
-                history,
-                location
-              )}
-              disabled={isOutOfStock}
-            >
-              {isBooking ? (
-                <FormattedMessage id="OrderPanel.ctaButtonMessageBooking" />
-              ) : isOutOfStock ? (
-                <FormattedMessage id="OrderPanel.ctaButtonMessageNoStock" />
-              ) : isPurchase ? (
-                <FormattedMessage id="OrderPanel.ctaButtonMakeOffer" />
-              ) : (
-                <FormattedMessage id="OrderPanel.ctaButtonMessageInquiry" />
-              )}
-            </SecondaryButton>
-          </div>
+          <PrimaryButton
+            onClick={handleSubmit(
+              isOwnListing,
+              isClosed,
+              showInquiryForm,
+              onSubmit,
+              history,
+              location
+            )}
+            disabled={isOutOfStock}
+          >
+            {isBooking ? (
+              <FormattedMessage id="OrderPanel.ctaButtonMessageBooking" />
+            ) : isOutOfStock ? (
+              <FormattedMessage id="OrderPanel.ctaButtonMessageNoStock" />
+            ) : isPurchase ? (
+              <FormattedMessage id="OrderPanel.ctaButtonMessagePurchase" />
+            ) : (
+              <FormattedMessage id="OrderPanel.ctaButtonMessageInquiry" />
+            )}
+          </PrimaryButton>
         )}
       </div>
-    </div>
+    </>
+
   );
 };
 
